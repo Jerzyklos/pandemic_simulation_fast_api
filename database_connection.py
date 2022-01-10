8e38fb5ff8700793ac8fa6d5020f3a03bd1a5cff
jerzyklos <jerzyklosik@interia.pl>
2022-01-10 16:52:42 +0100

    Added connection with database and upgraded simulation

---
+++ database_connection.py
@@ -0,0 +1,35 @@
+import pymongo
+from main import Person
+
+class Statistics:
+    pass
+
+class MongoDB:
+    def __init__(self, password):
+        url = "mongodb+srv://user0000:pass{}@cluster0.n0awd.mongodb.net/" \
+              "myFirstDatabase?retryWrites=true&w=majority".format(password)
+        self.client = pymongo.MongoClient(url)
+        self.db = self.client["simulation_db"]
+        self.collection = self.db["simulation_persons"]
+        self.collection = self.db["simulation_statistics"]
+    def delete_collection_content(self):
+        self.collection.delete_many({})
+    def add_person_to_col(self, person:Person):
+        pass
+    def add_stats_to_col(self, stats:Statistics):
+        pass
+    def return_record(self):
+        return self.collection.find_one()
+    def print_info(self):
+        print(self.client.list_database_names())
+        print(self.db.list_collection_names())
+
+
+mongoDB = MongoDB('XXXX')
+mongoDB.print_info()
+mongoDB.delete_collection_content()
+print(mongoDB.return_record())
+
+"""
+
+"""
---
+++ simulation_parameters.py
@@ -0,0 +1,27 @@
+# Simulation parameters
+
+# probability of ill person at the beginning of the simulation
+starting_illness_probability = 0.2
+# probability of having symptomps when ill
+# so asymptomatic probability = 1 - symptomatic_probability
+symptomatic_probability = 0.5
+# spreading probability when closer than spreading radius
+spreading_probability = 0.2
+spreading_radius = 2
+# recovery or death probability after minimal duration of illness
+min_duration_of_illness = 200
+recovery_probability = 0.005
+death_probability = 0.002
+# how long after illness person is immune to contracting illness
+min_immunity_duration = 200
+max_immunity_duration = 600
+# when quarantine is true, symptomatic persons don't move
+# and don't spread illness, when tested
+quarantine = False
+# availibity of tests, 1 means full avaibility
+test_avaibility = 0.9
+#dimensions of board
+X_DIM = 100
+Y_DIM = 100
+
+
